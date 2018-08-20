76a3cae535b94bc20b60257bd01c4649dd3a9093
Kanjiz1 <kanjis007@gmail.com>
2018-08-19 23:07:00 -0500

    Detecting taps on 3D Objects

--- ramp-up-app/Controller/RampPlacerVC.swift
+++ ramp-up-app/Controller/RampPlacerVC.swift
@@ -52,17 +52,6 @@ class RampPlacerVC: UIViewController {
         // Release any cached data, images, etc that aren't in use.
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
@@ -84,11 +73,16 @@ class RampPlacerVC: UIViewController {
     
     @IBAction func onRampBtnPressed(_ sender: UIButton) {
         let rampPickerVC = RampPickerVC(size: CGSize(width: 250, height: 500))
+        rampPickerVC.rampPlacerVC = self
         rampPickerVC.modalPresentationStyle = .popover
         rampPickerVC.popoverPresentationController?.delegate = self
         present(rampPickerVC, animated: true, completion: nil)
         rampPickerVC.popoverPresentationController?.sourceView = sender
         rampPickerVC.popoverPresentationController?.sourceRect = sender.bounds
+    }
+    
+    func onRampSelected(_ rampName: String){
+        
     }
     
 }
