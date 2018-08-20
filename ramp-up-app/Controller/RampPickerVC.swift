1f48d54f87599a33e448637dcc670b6998aba8f1
Kanjiz1 <kanjis007@gmail.com>
2018-08-19 22:27:45 -0500

    Putting 3D models in SceneKit

--- ramp-up-app/Controller/RampPickerVC.swift
+++ ramp-up-app/Controller/RampPickerVC.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
+import SceneKit
 
 class RampPickerVC: UIViewController {
 
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize){
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+    
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        var obj = SCNScene(named: "art.scnassets/pipe.dae")
+        var node = obj?.rootNode.childNode(withName: "pipe", recursively: true)
+        
+        node?.scale = SCNVector3Make(0.0022, 0.0022, 0.0022)
+        node?.position = SCNVector3Make(-1, 0.7, -1)
+        
+        scene.rootNode.addChildNode(node!)
+        preferredContentSize = size
+        
+        obj = SCNScene(named: "art.scnassets/pyramid.dae")
+        node = obj?.rootNode.childNode(withName: "pyramid", recursively: true)!
+        node?.scale = SCNVector3Make(0.0060, 0.0060, 0.0060)
+        node?.position = SCNVector3Make(-1, -0.450, -1)
+        
+        scene.rootNode.addChildNode(node!)
+        
+        obj = SCNScene(named: "art.scnassets/quarter.dae")
+        node = obj?.rootNode.childNode(withName: "quarter", recursively: true)!
+        node?.scale = SCNVector3Make(0.0058, 0.0058, 0.0058)
+        node?.position = SCNVector3Make(-1, -2.2, -1)
+        
+        scene.rootNode.addChildNode(node!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +66,4 @@ class RampPickerVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
